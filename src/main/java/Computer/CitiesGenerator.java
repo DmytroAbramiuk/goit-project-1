122249c94a8556f65d80f63f9e3e12779a179a21
Artem Ostapchuk <artem2050art@gmail.com>
2023-08-29 15:35:18 +0300

    CitiesGenerator
    added default class for cities generator

---
+++ src/main/java/Computer/CitiesGenerator.java
@@ -0,0 +1,13 @@
+package Computer;
+
+import java.util.Collection;
+
+public class CitiesGenerator {
+    private Collection <String> citiesForComputer;
+
+    public CitiesGenerator(Collection<String> citiesForComputer) {
+        this.citiesForComputer = citiesForComputer;
+    }
+
+    
+}
