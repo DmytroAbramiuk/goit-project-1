6c9d6c49a075d8c293a08c62166ca68ad9a2a958
DmytroAbramiuk <128092807+DmytroAbramiuk@users.noreply.github.com>
2023-08-29 17:37:07 +0300

    Start frame realization
    Added 2 labels with text:
    usernameLabel and citiesLabel.
    Also added start button and text field for username.
    Added initialize method.
    Added actionListener to method createStartButton.

---
+++ src/main/java/Interface/PanelForTheStartFrame.java
@@ -0,0 +1,103 @@
+package Interface;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PanelForTheStartFrame extends JPanel{
+
+    private final static int PANEL_WIDTH = 400;
+    private final static int PANEL_HEIGHT = 500;
+    private final static int START_BUTTON_HEIGHT = 35;
+    private final static int START_BUTTON_WIDTH = 130;
+    private final static int CITIES_LABEL_HEIGHT = 80;
+    private final static int CITIES_LABEL_WIDTH = 200;
+    private final static int USERNAME_FIELD_WIDTH = 170;
+    private final static int USERNAME_FIELD_HEIGHT = 35;
+    private final static int USERNAME_LABEL_WIDTH = 170;
+    private final static int USERNAME_LABEL_HEIGHT = 35;
+
+    public PanelForTheStartFrame(){
+        initialize();
+    }
+
+    private void initialize(){
+        this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+        this.setLayout(null);
+        this.setBackground(new Color(229, 171, 102, 186));
+
+        createStartButton();
+        createCitiesLabel();
+        createUsernameTextField();
+        createUsernameLabel();
+
+        this.setVisible(true);
+    }
+
+    private void createCitiesLabel(){
+        Font font = new Font("Arial Black", Font.BOLD, 50);
+        JLabel citiesLabel = new JLabel("CITIES");
+
+        citiesLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        citiesLabel.setFont(font);
+        citiesLabel.setBounds((PANEL_WIDTH/2)-(CITIES_LABEL_WIDTH/2),
+                10,
+                CITIES_LABEL_WIDTH,
+                CITIES_LABEL_HEIGHT);
+
+        this.add(citiesLabel);
+    }
+
+    private void createStartButton(){
+        JButton startButton = new JButton("START GAME");
+        Font font = new Font("Arial Black", Font.BOLD, 12);
+        startButton.setFont(font);
+
+        startButton.setBounds((PANEL_WIDTH/2)-(START_BUTTON_WIDTH/2),
+                (PANEL_HEIGHT/2)-(START_BUTTON_HEIGHT/2),
+                START_BUTTON_WIDTH,
+                START_BUTTON_HEIGHT);
+
+        startButton.setBorder(new LineBorder(Color.BLACK));
+        startButton.setBackground(new Color(229, 171, 102, 158));
+
+        startButton.addActionListener(e -> {
+            System.out.println("game was started!!!");
+        });
+
+        this.add(startButton);
+    }
+
+    private void createUsernameLabel(){
+        Font font = new Font("Arial Black", Font.BOLD, 15);
+        JLabel usernameLabel = new JLabel("Put your username");
+        usernameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        usernameLabel.setFont(font);
+        usernameLabel.setBounds((PANEL_WIDTH/2)-(USERNAME_LABEL_WIDTH/2),
+                PANEL_HEIGHT/2-(START_BUTTON_HEIGHT+USERNAME_LABEL_HEIGHT+USERNAME_FIELD_HEIGHT)+10,
+                USERNAME_FIELD_WIDTH,
+                USERNAME_LABEL_HEIGHT);
+
+        this.add(usernameLabel);
+    }
+
+    private void createUsernameTextField(){
+        JTextField usernameField = new JTextField();
+        usernameField.setBounds((PANEL_WIDTH/2)-(USERNAME_FIELD_WIDTH/2),
+                (PANEL_HEIGHT/2)-(USERNAME_FIELD_HEIGHT/2)-START_BUTTON_HEIGHT-10,
+                USERNAME_FIELD_WIDTH,
+                USERNAME_FIELD_HEIGHT);
+
+        Font font = new Font("Arial Black", Font.BOLD, 20);
+        usernameField.setFont(font);
+
+        usernameField.setBorder(new LineBorder(Color.BLACK));
+        usernameField.setBackground(new Color(229, 171, 102, 158));
+
+        this.add(usernameField);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import Interface.StartFrame;
+
 public class Main {
     public static void main(String[] args) {
+        new StartFrame();
         //do not test your classes here, create temp classes for testing your code.
     }
 }
