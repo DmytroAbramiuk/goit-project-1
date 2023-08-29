2e1e0902295d6ef24f6125dcd42a880b6b49077a
DmytroAbramiuk <128092807+DmytroAbramiuk@users.noreply.github.com>
2023-08-29 13:54:58 +0300

    Added GameFrame
    added default settings for a game frame.

--- src/main/java/Interface/GameFrame.java
+++ src/main/java/Interface/GameFrame.java
@@ -1,4 +1,11 @@
 package Interface;
 
-public class GameFrame {
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    public GameFrame(){
+        this.setSize(400,500);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
 }
