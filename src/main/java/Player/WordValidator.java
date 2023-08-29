4bd72e4996efe431258dd90e69ebcda955450100
Virus-21 <55316181+Virus-21@users.noreply.github.com>
2023-08-29 23:12:21 +0300

    Added FileReader. created WordValidator
    wordValidator is done.

--- src/main/java/Player/WordValidator.java
+++ src/main/java/Player/WordValidator.java
@@ -1,5 +1,38 @@
 package Player;
 
+import java.util.Collection;
+
 public class WordValidator {
-    private String word;
+    public static boolean validate(Collection<String> allCities,Collection<String> usedWords ,
+                                   String playerWord, String computerWord){
+        computerWord = computerWord.toLowerCase();
+        playerWord = playerWord.toLowerCase();
+        System.out.println(allCities);
+        if(computerWord.equals("firstword")){
+            return true;
+        }
+        if(isWordInList(usedWords,playerWord)){
+            return false;
+        }
+
+        if(playerWord.charAt(0) == computerWord.charAt(computerWord.length()-1)){
+            if(isWordInList(allCities,playerWord)) {
+                // Here we must add that word to used WordList
+                //
+                return true;
+            }
+        }
+        return false;
+    }
+    private static boolean isWordInList(Collection<String> usedWords ,String playerWord){
+        for (String usedWord:usedWords
+             ) {
+            if(usedWord.hashCode() == playerWord.hashCode()){
+                if(usedWord.equals(playerWord)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
