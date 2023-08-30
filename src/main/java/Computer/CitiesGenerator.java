32fe2fd48726db905b4a43f06ea9ada391b479e0
Artem Ostapchuk <artem2050art@gmail.com>
2023-08-30 15:21:34 +0300

    CitiesGeneratorFinal
    added the generateRandomCities method, which generates a list of random cities, the getCitiesForComputer method returns the original list of cities that was passed in the constructor

--- src/main/java/Computer/CitiesGenerator.java
+++ src/main/java/Computer/CitiesGenerator.java
@@ -1,13 +1,35 @@
 package Computer;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Random;
 
 public class CitiesGenerator {
-    private Collection <String> citiesForComputer;
+    private Collection<String> citiesForComputer;
 
     public CitiesGenerator(Collection<String> citiesForComputer) {
         this.citiesForComputer = citiesForComputer;
     }
 
-    
+    public List<String> generateRandomCities() {
+        List<String> randomCities = new ArrayList<>(citiesForComputer);
+        List<String> selectedCities = new ArrayList<>();
+        Random random = new Random();
+
+        int numberOfCitiesToSelect = 200;
+
+        while (selectedCities.size() < numberOfCitiesToSelect && !randomCities.isEmpty()) {
+            int randomIndex = random.nextInt(randomCities.size());
+            selectedCities.add(randomCities.remove(randomIndex));
+        }
+
+        return selectedCities;
+    }
+
+    public Collection<String> getCitiesForComputer() {
+        return citiesForComputer;
+    }
 }
+
+
