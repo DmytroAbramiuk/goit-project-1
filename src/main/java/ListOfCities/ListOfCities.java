26c6d34c6b762c9b1d29fa953b060ac7d7e70d3b
Virus-21 <55316181+Virus-21@users.noreply.github.com>
2023-08-30 18:39:13 +0300

    Fixed Error

--- src/main/java/ListOfCities/ListOfCities.java
+++ src/main/java/ListOfCities/ListOfCities.java
@@ -3,9 +3,10 @@ package ListOfCities;
 import FileReader.FileReader;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListOfCities {
-    public static List<String> usedWords;
+    public static List<String> usedWords = new ArrayList<>();
     public static List<String> allCities = FileReader.readFile(new File("src/main/java/Files/Cities.txt"));
 }
